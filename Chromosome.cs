02e4adb955555cec9d13f33526acd044fa20b847
KarolPiechaczek <piechaczekkarol1@gmail.com>
2025-10-15 00:51:44 +0200

    Update Chromosome and basic changes in Program.cs

--- Chromosome.cs
+++ Chromosome.cs
@@ -10,7 +10,7 @@ namespace Genetic_Algorithm
     {
         private List<Gene> _genes;
         private List<bool> _isGenePresent;
-        private int _fitnessValue = 0;
+        private int _fitnessValue;
 
         public List<Gene> Genes
         {
@@ -27,10 +27,48 @@ namespace Genetic_Algorithm
             get { return _fitnessValue; }
             set { _fitnessValue = value; }
         }
-        public Chromosome(List<Gene> genes, List<bool> isGenePresent)
+        public Chromosome(List<Gene> genes, int fitnessValue)
         {
             _genes = genes;
-            _isGenePresent = isGenePresent;
+            _isGenePresent = RandomizePresence(genes.Count);
+            _fitnessValue = CalculateFitness(genes, _isGenePresent);
+        }
+
+        public int CalculateFitness(List<Gene> genes, List<bool> isGenePresent)
+        {
+            int fitnessValue = 0;
+            int weightValue = 0;
+            for (int i = 0; i < genes.Count; i++)
+            {
+                if (isGenePresent[i] == true)
+                {
+                    fitnessValue += genes[i].Value;
+                    weightValue += genes[i].Weight;
+                }
+            }
+            if (weightValue > 1500)
+            {
+                fitnessValue = 0;
+            }
+            return fitnessValue;
+        }
+
+        public List<bool> RandomizePresence(int quantityOfGenes)
+        {
+            List<bool> isGenePresent = new List<bool>();
+            for (int i = 0; i < quantityOfGenes; i++)
+            {
+                Random random = new Random();
+                if (random.Next(2) % 2 == 0)
+                {
+                    isGenePresent.Add(false);
+                }
+                else
+                {
+                    isGenePresent.Add(true);
+                }
+            }
+            return isGenePresent;
         }
     }
 }
--- Population.cs
+++ Population.cs
@@ -9,8 +9,6 @@ namespace Genetic_Algorithm
     class Population
     {
         private List<Chromosome> _chromosomes;
-        private int _populationSize;
-        private int _maxNumberOfIterations;
         private int _currentIteration;
 
         public List<Chromosome> Chromosomes
@@ -18,26 +16,14 @@ namespace Genetic_Algorithm
             get { return _chromosomes; }
             set { _chromosomes = value; }
         }
-        public int PopulationSize
-        {
-            get { return _populationSize; }
-            set { _populationSize = value; }
-        }
-        public int MaxNumberOfIterations
-        {
-            get { return _maxNumberOfIterations; }
-            set { _maxNumberOfIterations = value; }
-        }
         public int CurrentIteration
         {
             get { return _currentIteration; }
             set { _currentIteration = value; }
         }
-        public Population(List<Chromosome> chromosomes, int populationSize, int maxNumberOfIterations, int currentIteration)
+        public Population(List<Chromosome> chromosomes, int currentIteration)
         {
             _chromosomes = chromosomes;
-            _populationSize = populationSize;
-            _maxNumberOfIterations = maxNumberOfIterations;
             _currentIteration = currentIteration;
         }
     }
