8256cfd76c157f8866575a564eba5f06f8e56d61
KarolPiechaczek <piechaczekkarol1@gmail.com>
2025-10-14 23:36:53 +0200

    Class Chromosome added

---
+++ Chromosome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_Algorithm
+{
+    class Chromosome
+    {
+        private List<Gene> _genes;
+        private List<bool> _isGenePresent;
+        private int _fitnessValue = 0;
+
+        public List<Gene> Genes
+        {
+            get { return _genes; }
+            set { _genes = value; }
+        }
+        public List<bool> IsGenePresent
+        {
+            get { return _isGenePresent; }
+            set { _isGenePresent = value; }
+        }
+        public int FitnessValue
+        {
+            get { return _fitnessValue; }
+            set { _fitnessValue = value; }
+        }
+        public Chromosome(List<Gene> genes, List<bool> isGenePresent)
+        {
+            _genes = genes;
+            _isGenePresent = isGenePresent;
+        }
+    }
+}
